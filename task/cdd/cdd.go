7c00cb5ce9a8137cae871b793b783ab1ef972f3a
samuelmantou <samuelmantou@gmail.com>
2022-03-28 20:26:50 +0800

    a

--- task/cdd/cdd.go
+++ task/cdd/cdd.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
+	"github.com/chromedp/cdproto/network"
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
+	"io/ioutil"
+	"log"
 	"mxb/task"
+	"time"
 )
 
 type job struct {
@@ -19,7 +23,33 @@ func (j *job) Login(ctx context.Context, data chan<- string) error {
 	var buf []byte
 	err := chromedp.Run(ctx,
 		chromedp.WaitEnabled(sel),
+		chromedp.Sleep(time.Second * 3),
 		chromedp.Screenshot(sel, &buf, chromedp.NodeVisible),
+		chromedp.WaitVisible(`#mms-header-next > div.mms-header-container > div > div.mms-header__list > a:nth-child(1) > div > span`),
+		chromedp.Tasks{
+			chromedp.ActionFunc(func(ctx context.Context) error {
+				cookies, err := network.GetAllCookies().Do(ctx)
+				if err != nil {
+					return err
+				}
+				for _, cookie := range cookies {
+					log.Printf("%+v\n", cookie)
+				}
+
+				// 2. 序列化
+				cookiesData, err := network.GetAllCookiesReturns{Cookies: cookies}.MarshalJSON()
+				if err != nil {
+					return err
+				}
+
+				// 3. 存储到临时文件
+				if err = ioutil.WriteFile("cookies.tmp", cookiesData, 0755); err != nil {
+					return err
+				}
+
+				return nil
+			}),
+		},
 	)
 	if err == nil {
 		imageBase64 := "data:image/png;base64," + base64.StdEncoding.EncodeToString(buf)
