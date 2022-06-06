85b4a0f26453e052acf21598673bc82ac32f0f62
samuelmantou <samuelmantou@gmail.com>
2022-06-06 21:56:06 +0800

    a

--- cmd/sql/main.go
+++ cmd/sql/main.go
@@ -3,17 +3,43 @@ package main
 import (
 	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
+	"log"
+	"net/url"
+	"time"
 )
 
 func main() {
-	dsn := "sqlserver://sa:mxb.1234@localhost:1433?database=AIS20220601165723"
-	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
+	query := url.Values{}
+	query.Add("database", "AIS20220601165723")
+	query.Add("encrypt", "disable")
+	u := &url.URL{
+		Scheme:   "sqlserver",
+		User:     url.UserPassword("sa", "mxb.1234"),
+		Host:     "localhost:1433",
+		RawQuery: query.Encode(),
+	}
+	db, err := gorm.Open(sqlserver.Open(u.String()), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 	sqlDb, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
 	err = sqlDb.Ping()
 	if err != nil {
 		panic(err)
+	}
+	rows, err := db.Raw("Select Top 5 FDate FROM ICStockBill ORDER BY FInterID DESC").Rows()
+	defer rows.Close()
+	if err != nil {
+		panic(err)
+	}
+	for rows.Next() {
+		var date time.Time
+		if err := rows.Scan(&date); err != nil {
+			log.Println(err)
+		}
+		log.Println(date)
 	}
 }
